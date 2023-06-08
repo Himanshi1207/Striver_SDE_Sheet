c258887cfda041f077d3b55811948a3db8ea9cad
Himanshi <kanojiahimanshi1221@gmail.com>
2023-06-09 00:36:57 +0530

    5th Day Done

---
+++ 27_Merge_Two_Sorted_Lists.cpp
@@ -0,0 +1,49 @@
+// TC-O(N)
+// SC-O(N)
+#include <bits/stdc++.h>
+using namespace std;
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution
+{
+public:
+    ListNode *mergeTwoLists(ListNode *p1, ListNode *p2)
+    {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *p3 = dummy;
+        while (p1 != NULL and p2 != NULL)
+        {
+            if (p1->val < p2->val)
+            {
+                p3->next = p1;
+                p1 = p1->next;
+            }
+            else
+            {
+                p3->next = p2;
+                p2 = p2->next;
+            }
+            p3 = p3->next;
+        }
+        while (p1 != NULL)
+        {
+            p3->next = p1;
+            p1 = p1->next;
+            p3 = p3->next;
+        }
+        while (p2 != NULL)
+        {
+            p3->next = p2;
+            p2 = p2->next;
+            p3 = p3->next;
+        }
+        return dummy->next;
+    }
+};
